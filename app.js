8d76aa8018e2da81be0ab453861aff42eb0fae85
LuckyProut <luc.garrouste@gmail.com>
2024-11-12 16:07:16 +0100

    add get one book

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-// const Book = require('./models/book');
+
 const booksRoutes = require('./routes/books');
 
 const app = express();
@@ -10,18 +10,8 @@ mongoose.connect('mongodb+srv://lucgarrouste:Lucio_68420@cluster0.8psgz.mongodb.
   .catch((err) => console.log('Connexion à MongoDB échouée !', err));
 
   app.use(cors({ origin: 'http://localhost:3000' }));
-  // app.use('/api/books', (req, res, next) => {
-  //   Book.find()
-  //     .then(books => res.status(200).json(books))
-  //     .catch(error => res.status(400).json({ error }));
-  // });
 
-  // app.use('/api/books/:id', (req, res, next) => {
-  //   bookSchema.findOne({ id: req.params.id })
-  //     .then(things => res.status(200).json(things))
-  //     .catch(error => res.status(400).json({ error }));
-  // });
-
+ 
   app.use('/api/books', booksRoutes);
   
-module.exports = app;
+module.exports = app; 
