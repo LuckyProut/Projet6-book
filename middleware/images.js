253ab9597c8d163edb57367edf29f0631927dc54
LuckyProut <luc.garrouste@gmail.com>
2024-11-22 16:17:48 +0100

    fix images middleware correction

--- middleware/images.js
+++ middleware/images.js
@@ -1,7 +1,4 @@
 const multer = require('multer');
-const sharp = require('sharp');
-const path = require('path');
-const fs = require('fs');
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -26,30 +23,3 @@ const storage = multer.diskStorage({
 
 // Gestion des téléchargements de fichiers image uniquement
 module.exports = multer({storage: storage}).single('image');
-
-// Redimensionnement de l'image
-module.exports.resizeImage = (req, res, next) => {
-  // On vérifie si un fichier a été téléchargé
-  if (!req.file) {
-    return next();
-  }
-
-  const filePath = req.file.path;
-  const fileName = req.file.filename;
-  const outputFilePath = path.join('images', `resized_${fileName}`);
-
-  sharp(filePath)
-    .resize({ width: 206, height: 260 })
-    .toFile(outputFilePath)
-    .then(() => {
-      // Remplacer le fichier original par le fichier redimensionné
-      fs.unlink(filePath, () => {
-        req.file.path = outputFilePath;
-        next();
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      return next();
-    });
-};
